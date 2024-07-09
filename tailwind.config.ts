8c2853773f0c2ac4191149347e01cf9830618a4b
ryanfavour4 <ryanfave022@gmail.com>
2024-07-09 13:45:59 +0100

    VERSION 1 FOR REFERENCE AND MOSTLY SAVING THE LAYOUT META TAGS OBJECT

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
+import { KeyValuePair, ResolvableTo } from 'tailwindcss/types/config'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        black: '#000000',
+        white: '#FFFFFF',
+        primary: '#0080DE',
+        secondary: '#E6F4F1',
+        complementary: '#404756',
+        greytext: '#525560',
+        error: '#D92D20',
+        warning: '#FDB022',
+        success: '#054F31',
+        info: '#0E36FF',
+      },
+      translate: ['group-hover', 'hover'] as unknown as ResolvableTo<KeyValuePair<string, string>>,
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
