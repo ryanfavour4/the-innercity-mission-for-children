9ef7027f73adc31ad05a75cb9265d6f1b0ffc3c4
FAITH <faithokereke26@gmail.com>
2024-07-13 18:44:32 +0100

    changes made'

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,23 +6,23 @@ import { TiSocialLinkedin } from "react-icons/ti";
 export default function AboutUs(){
     return(
     <div className="pt-10 pb-10">
-     <div className="flex items-center justify-normal align-middle gap-2  pl-8">
+     <div className="flex items-center justify-normal align-middle gap-2  pl-4 md:pl-8">
                 <div className="border-2 border-black w-10 border-l-0 border-r-0 border-b-0 mb-5"></div>
-            <h3 className="uppercase pb-6 font-bold">KNOW ABOUT US</h3>
+            <h3 className="uppercase md:pb-6 pb-3 font-bold">KNOW ABOUT US</h3>
             </div>
-      <div className="flex justify-between gap-8 font-bold pr-20 pl-20">
-        <h1 className="text-3xl">We are the Inner city <br /> Mission for Children</h1>
+      <div className=" md:flex-row flex-col flex justify-between md:gap-8 gap-2 font-bold md:pr-20 md:pl-20 pl-6 pr-6" >
+        <h1 className="md:text-4xl text-2xl">We are the Inner city Mission for Children</h1>
         <div>
-          <p className="pt-2">
-            But Jesus said, suffer little children, and forbid them not, <br /> to come
+          <p className="pt-2 md:text-sm text-xs">
+            But Jesus said, suffer little children, and forbid them not, to come
             unto me: for of such is the kingdom of heaven
           </p>
-          <p className="font-extrabold pt-2">Matthew 19:14 KJV</p>
+          <p className="font-extrabold pt-2 md:block flex items-end justify-end text-right text-xs md:text-sm">Matthew 19:14 KJV</p>
         </div>
       </div>
-      <Image width={100} height={100} className="rounded-md mt-10 mb-10  ml-16 mr-16 w-[90%]" src= "/assets/images/BG.png"alt=""/>
-      <div className="pl-32 pr-32 flex flex-col gap-3 pb-8">
-        <h2 className="text-3xl font-bold">Introduction</h2>
+      <Image width={100} height={100} className="rounded-md mt-10 mb-10 ml-6 mr-0 md:ml-16 md:mr-16 w-[90%]" src= "/assets/images/BG.png"alt=""/>
+      <div className="md:pl-32 md:pr-32 pr-6 pl-6 flex flex-col gap-3 pb-8">
+        <h2 className="md:text-3xl text-xl font-bold">Introduction</h2>
         <p className="text-sm text-gray-500">
           The InnerCity Mission was birthed in December 2004 after the Founder,
           Rev Chris Oyakhilome (D.Sc., D.Sc., D.D.), the President of LoveWorld
@@ -45,14 +45,14 @@ export default function AboutUs(){
           country in 2005, to over 100 countries in 2022.
         </p>
       </div>
-      <div className="bg-lightGreen flex justify-around p-32 items-center">
+      <div className="bg-lightGreen md:flex-row flex-col pl-6 pr-6 pt-10 pb-10 flex justify-around md:p-32 items-center">
         <div>
           <h2 className="uppercase font-bold tracking-wide text-sm pb-3">OUR MISSION</h2>
           <h1 className="font-bold text-xl">
-            Ensure every child thrives, escapes <br /> poverty, & contributes to
+            Ensure every child thrives, escapes poverty, & contributes to
             progress.
           </h1>
-          <p className="w-[65%] text-sm pt-4"> 
+          <p className="md:w-[65%] text-sm pt-4"> 
             To create an environment where every child survives, lives above
             poverty, and becomes an active participant in socio-economic
             development irrespective of race, color, or gender. Through our
@@ -62,7 +62,7 @@ export default function AboutUs(){
             care and sustained solutions to issues caused by poverty.
           </p>
         </div>
-        <div>
+        <div className="pt-4 md:p-0">
           <h2 className="uppercase font-bold tracking-wide text-sm pb-3">OUR VISION</h2>
           <h1 className="font-bold text-xl">A poverty-free world where every child can thrive.</h1>
           <p className=" text-sm pt-4">
@@ -72,8 +72,8 @@ export default function AboutUs(){
         </div>
       </div>
 
-      <div className="pt-10 pb-10 pl-36 pr-36">
-        <p className="italic font-bold text-sm text-center text-gray">
+      <div className="md:pt-10 md:pb-10 md:pl-36 md:pr-36 p-4" >
+        <p className="italic font-bold md:text-sm text-xs text-center text-gray">
           Remember, every child is your child! Help children, don’t ignore
           children. They are our extension; they are our future. No matter how
           dirty or untouchable they are, think again. You were just that small
@@ -84,16 +84,16 @@ export default function AboutUs(){
           small as they are, as needy as they are, as frustrated as they are,
           they have dreams. We can help make those dreams come true!
         </p>
-        <p className="font-bold text-gray text-center pt-6 text-lg">
+        <p className="font-bold text-grey text-sm text-center pt-6 md:text-lg">
           Founder, The InnerCity Mission: Rev Dr. Chris Oyakhilome (D.Sc.,
           D.Sc., D.D.) 2004
         </p>
       </div>
-      <div className="bg-lightGreen p-16 grid grid-cols-2">
-        <h1 className="text-4xl font-bold pb-8">OUR CORE VALUES</h1>
+      <div className="bg-lightGreen md:p-16 grid md:grid-cols-2 grid-cols-1 p-2">
+        <h1 className="md:text-4xl text-xl font-bold pb-8 pl-5 md:pl-0 pt-4 md:pt-0">OUR CORE VALUES</h1>
             <div className="border-2 border-gray border-b-0  border-r-0 border-t-0"></div>
             <div>
-          <div className="flex items-start align-top justify-normal gap-3 p-8">
+          <div className="flex items-start align-top justify-normal gap-3 p-4">
             <Image width={100} height={100} className="w-6" src="/assets/images/Icon.png" alt="" />
             <div>
               <p className="font-bold">We are a people of Faith</p>
@@ -107,7 +107,7 @@ export default function AboutUs(){
           </div>
         </div>
         <div>
-          <div className="flex items-start align-top justify-normal gap-3 p-8">
+          <div className="flex items-start align-top justify-normal gap-3 p-4">
             <Image width={100} height={100} className="w-6" src="/assets/images/Icon.png" alt="" />
             <div>
               <p className="font-bold">We are a community of Hope</p>
@@ -118,18 +118,18 @@ export default function AboutUs(){
           </div>
         </div>
         <div>
-          <div className="flex items-start align-top justify-normal gap-3 p-8">
+          <div className="flex items-start align-top justify-normal gap-3 p-4">
             <Image width={100} height={100} className="w-6" src="/assets/images/Icon.png" alt="" />
             <div>
               <p className="font-bold">We are Accountable</p>
               <p  className="text-gray">
-                OWe account for our activities, accept responsibility for them, and transparently share results.
+                We account for our activities, accept responsibility for them, and transparently share results.
               </p>
             </div>
           </div>
         </div>
         <div>
-          <div className="flex items-start align-top justify-normal gap-3 p-8">
+          <div className="flex items-start align-top justify-normal gap-3 p-4">
             <Image width={100} height={100} className="w-6" src="/assets/images/Icon.png" alt="" />
             <div>
               <p className="font-bold">We are Servants of Love</p>
@@ -140,7 +140,7 @@ export default function AboutUs(){
           </div>
         </div>
         <div>
-          <div className="flex items-start align-top justify-normal gap-3 p-8">
+          <div className="flex items-start align-top justify-normal gap-3 p-4">
             <Image width={100} height={100} className="w-6" src="/assets/images/Icon.png" alt="" />
             <div>
               <p className="font-bold">We build Authentic Relationships</p>
@@ -151,7 +151,7 @@ export default function AboutUs(){
           </div>
         </div>
         <div>
-          <div className="flex items-start align-top justify-normal gap-3 p-8">
+          <div className="flex items-start align-top justify-normal gap-3 p-4">
             <Image width={100} height={100} className="w-6" src="/assets/images/Icon.png" alt="" />
             <div>
               <p className="font-bold">We model Respect and Integrity</p>
@@ -164,99 +164,99 @@ export default function AboutUs(){
       </div>
       <div className="flex flex-col justify-center items-center pb-20">
         <h1 className="text-3xl font-bold p-7">Our programmatic areas</h1>
-        <div className="grid grid-cols-3 gap-8">
-          <div className="flex gap-2 items-center pl-16 pr-16">
-            <Image width={100} height={100} className="bg-blue rounded-md w-10 p-2" src="/assets/images/education.png" alt="" />
-            <p className="font-bold">Education</p>
+        <div className="grid md:grid-cols-3 grid-cols-2 md:gap-8">
+          <div className="flex gap-2 items-center md:pl-16 md:pr-16 p-4">
+            <Image width={100} height={100} className="bg-primary rounded-md w-10 p-2" src="/assets/images/education.png" alt="" />
+            <p className="font-bold text-xs md:text-sm">Education</p>
           </div>
-          <div className="flex gap-2 items-center pl-16 pr-16">
-            <Image width={100} height={100} className="bg-blue rounded-md w-10 p-2" src="/assets/images/education.png" alt="" />
-            <p className="font-bold">Feeding</p>
+          <div className="flex gap-2 items-center md:pl-16 md:pr-16 p-4">
+            <Image width={100} height={100} className="bg-primary rounded-md w-10 p-2" src="/assets/images/education.png" alt="" />
+            <p className="font-bold text-xs md:text-sm">Feeding</p>
           </div>
-          <div className="flex gap-2 items-center pl-16 pr-16">
-            <Image width={100} height={100} className="bg-blue rounded-md w-10 p-2" src="/assets/images/education.png" alt="" />
-            <p className="font-bold">Family Strenghtening and Livelihood</p>
+          <div className="flex gap-2 items-center md:pl-16 md:pr-16 p-4">
+            <Image width={100} height={100} className="bg-primary rounded-md w-10 p-2" src="/assets/images/education.png" alt="" />
+            <p className="font-bold text-xs md:text-sm">Family Strenghtening and Livelihood</p>
           </div>
-          <div className="flex gap-2 items-center pl-16 pr-16">
-            <Image width={100} height={100} className="bg-blue rounded-md w-10 p-2" src="/assets/images/education.png" alt="" />
-            <p className="font-bold">Shelter</p>
+          <div className="flex gap-2 items-center md:pl-16 md:pr-16 p-4">
+            <Image width={100} height={100} className="bg-primary rounded-md w-10 p-2" src="/assets/images/education.png" alt="" />
+            <p className="font-bold text-xs md:text-sm">Shelter</p>
           </div>
-          <div className="flex gap-2 items-center pl-16 pr-16">
-            <Image width={100} height={100} className="bg-blue rounded-md w-10 p-2" src="/assets/images/education.png" alt="" />
-            <p className="font-bold">Humanitarian Response</p>
+          <div className="flex gap-2 items-center md:pl-16 md:pr-16 p-4">
+            <Image width={100} height={100} className="bg-primary rounded-md w-10 p-2" src="/assets/images/education.png" alt="" />
+            <p className="font-bold text-xs md:text-sm">Humanitarian Response</p>
           </div>
-          <div className="flex gap-2 items-center pl-16 pr-16">
-            <Image width={100} height={100} className="bg-blue rounded-md w-10 p-2" src="/assets/images/education.png" alt="" />
-            <p className="font-bold">Health and Nutrition</p>
+          <div className="flex gap-2 items-center md:pl-16 md:pr-16 p-4">
+            <Image width={100} height={100} className="bg-primary rounded-md w-10 p-2" src="/assets/images/education.png" alt="" />
+            <p className="font-bold text-xs md:text-sm">Health and Nutrition</p>
           </div>
-          <div className="flex gap-2 items-center pl-16 pr-16">
-            <Image width={100} height={100} className="bg-blue rounded-md w-10 p-2" src="/assets/images/education.png" alt="" />
-            <p className="font-bold">Child Safety and Advocacy</p>
+          <div className="flex gap-2 items-center md:pl-16 md:pr-16 p-4">
+            <Image width={100} height={100} className="bg-primary rounded-md w-10 p-2" src="/assets/images/education.png" alt="" />
+            <p className="font-bold text-xs md:text-sm">Child Safety and Advocacy</p>
           </div>
-          <div className="flex gap-2 items-center pl-16 pr-16">
-            <Image width={100} height={100} className="bg-blue rounded-md w-10 p-2" src="/assets/images/education.png" alt="" />
-            <p className="font-bold">Faith and Development</p>
+          <div className="flex gap-2 items-center md:pl-16 md:pr-16 p-4">
+            <Image width={100} height={100} className="bg-primary rounded-md w-10 p-2" src="/assets/images/education.png" alt="" />
+            <p className="font-bold text-xs md:text-sm">Faith and Development</p>
           </div>
         </div>
       </div>
       <div >
         <div className="flex items-center justify-normal align-middle gap-2 pl-6">
           <div className="border-2 w-10 border-l-0 border-r-0 border-b-0 mb-5"></div>
-          <h3 className="uppercase pb-6 font-bold">Meet out team</h3>
+          <h3 className="uppercase md:pb-6 pb-2 font-bold">Meet out team</h3>
         </div>
-        <h1 className="pl-16 font-bold text-4xl">Board of Directors</h1>
-        <div className="p-16 grid grid-cols-4">
+        <h1 className="md:pl-16 pl-6 font-bold md:text-4xl text-2xl">Board of Directors</h1>
+        <div className="md:p-16 m-10 grid md:grid-cols-4 grid-cols-2 gap-5 md:gap-5">
           <div className="flex flex-col items-center justify-center gap-1 pb-3">
             <Image width={100} height={100} className="w-[200px]" src="/assets/images/avatar1.png" alt="" />
             <p className="font-bold text-sm">Leonard John Davis</p>
             <p className="text-xs">Cofounder, CEO</p>
-            <Image width={100} height={100} className="w-32 bg-blue p-1 rounded-sm" src="/assets/images/Socials.png" alt="" />
+            <Image width={100} height={100} className="w-32 bg-primary p-1 rounded-sm" src="/assets/images/Socials.png" alt="" />
           </div>
           <div className="flex flex-col items-center justify-center gap-1 pb-3">
             <Image width={100} height={100} className="w-[200px]" src="/assets/images/avatar2.png" alt="" />
             <p className="font-bold text-sm">Francis Weber</p>
             <p className="text-xs">Vice chairman</p>
-            <Image width={100} height={100} className="w-32 bg-blue p-1 rounded-sm" src="/assets/images/Socials.png" alt="" />
+            <Image width={100} height={100} className="w-32 bg-primary p-1 rounded-sm" src="/assets/images/Socials.png" alt="" />
           </div>
           <div className="flex flex-col items-center justify-center gap-1 pb-3">
             <Image width={100} height={100} className="w-[200px]" src="/assets/images/avatar.png" alt="" />
             <p className="font-bold text-sm">Kyla Orien</p>
             <p className="text-xs">Head of Authority</p>
-            <Image width={100} height={100} className="w-32 bg-blue p-1 rounded-sm" src="/assets/images/Socials.png" alt="" />
+            <Image width={100} height={100} className="w-32 bg-primary p-1 rounded-sm" src="/assets/images/Socials.png" alt="" />
           </div>
           <div className="flex flex-col items-center justify-center gap-1 pb-3">
             <Image width={100} height={100} className="w-[200px]" src="/assets/images/avatar3.png" alt="" />
             <p className="font-bold text-sm">Adrian Dixon</p>
             <p className="text-xs">Support Authority</p>
-            <Image width={100} height={100} className="w-32 bg-blue p-1 rounded-sm" src="/assets/images/Socials.png" alt="" />
+            <Image width={100} height={100} className="w-32 bg-primary p-1 rounded-sm" src="/assets/images/Socials.png" alt="" />
           </div>
           <div className="flex flex-col items-center justify-center gap-1 pb-3">
             <Image width={100} height={100} className="w-[200px]" src="/assets/images/avatar4.png" alt="" />
             <p className="font-bold text-sm">Freddy Busby</p>
             <p className="text-xs">Project Manager</p>
-            <Image width={100} height={100} className="w-32 bg-blue p-1 rounded-sm" src="/assets/images/Socials.png" alt="" />
+            <Image width={100} height={100} className="w-32 bg-primary p-1 rounded-sm" src="/assets/images/Socials.png" alt="" />
           </div>
           <div className="flex flex-col items-center justify-center gap-1 pb-3">
             <Image width={100} height={100} className="w-[200px]" src="/assets/images/avatar5.png" alt="" />
             <p className="font-bold text-sm">Kalu Alu</p>
             <p className="text-xs">Head of Authority</p>
-            <Image width={100} height={100} className="w-32 bg-blue p-1 rounded-sm" src="/assets/images/Socials.png" alt="" />
+            <Image width={100} height={100} className="w-32 bg-primary p-1 rounded-sm" src="/assets/images/Socials.png" alt="" />
           </div>
           <div className="flex flex-col items-center justify-center gap-1 pb-3">
             <Image width={100} height={100} className="w-[200px]" src="/assets/images/avatar6.png" alt="" />
             <p className="font-bold text-sm">Kalu Alu</p>
             <p className="text-xs">Head of Authority</p>
-            <Image width={100} height={100} className="w-32 bg-blue p-1 rounded-sm" src="/assets/images/Socials.png" alt="" />
+            <Image width={100} height={100} className="w-32 bg-primary p-1 rounded-sm" src="/assets/images/Socials.png" alt="" />
           </div>   
       </div>
     </div>
     <div>
-        <h1 className="pl-16 font-bold text-4xl">Executive director</h1>
-        <div className="flex flex-col align-middle p-16 text-center items-center">
+        <h1 className="md:pl-16 pl-0 text-center md:text-justify font-bold md:text-4xl text-3xl">Executive director</h1>
+        <div className="flex flex-col  align-middle md:p-16 p-6 text-center items-center md:items-start">
             <Image width={100} height={100} className="w-[200px]" src="/assets/images/avatar.png" alt="" />
             <p className="font-bold text-lg">Kalu Alu</p>
             <p className="text-xs">Head of Authority</p>
-            <Image width={100} height={100} className="w-32 bg-blue p-1 rounded-sm" src="/assets/images/Socials.png" alt="" />
+            <Image width={100} height={100} className="w-32 bg-primary p-1 rounded-sm" src="/assets/images/Socials.png" alt="" />
           </div>   
     </div>
     </div>
--- layouts/landing-page-navbar/index.tsx
+++ layouts/landing-page-navbar/index.tsx
@@ -1,59 +1,95 @@
 'use client'
-import { CloseXIcon, MenuFriesIcon } from '@/components/svgs'
+import { CloseXIcon, MenuFriesIcon, SearchIcon } from '@/components/svgs'
 /* eslint-disable @next/next/no-img-element */
-import Image from "next/image"
-import React, { useState } from 'react'
-import { CiSearch } from "react-icons/ci";
-import { MdKeyboardArrowDown } from "react-icons/md";
-import Link from 'next/link';
-import { RxHamburgerMenu } from "react-icons/rx";
+import Image from 'next/image'
+import React, { useState, useEffect } from 'react'
+import { CiSearch } from 'react-icons/ci'
+import { MdKeyboardArrowDown } from 'react-icons/md'
+import Link from 'next/link'
+import { RxHamburgerMenu } from 'react-icons/rx'
+import { ChevronDownIcon } from '@/components/svgs'
 export default function LandingPageNavbar() {
   const [isNavOpen, setIsNavOpen] = useState(false)
+  const [isMobile, setIsMobile] = useState(true)
 
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen)
   }
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768)
+    }
+
+    window.addEventListener('resize', handleResize)
+    handleResize()
+
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
 
   return (
-    <div>
-       <div className='md:hidden'>
-      <RxHamburgerMenu />
+    <div className="items-center justify-between gap-10 border-b-2 bg-white md:flex md:justify-normal md:gap-32 md:border-0  md:bg-primary md:pb-3 md:pl-16 md:pr-16 md:pt-3 ">
+      <div className="flex items-center justify-between p-3">
+        <div className="md:hidden cursor-pointer">
+          {!isNavOpen ? (
+            <MenuFriesIcon onClick={toggleNav} width="60px" />
+          ) : (
+            <CloseXIcon
+              className="fill-black"
+              onClick={toggleNav}
+              width="60px"
+              height="20px"
+              color="#0080DE"
+            />
+          )}
+        </div>
+        <div>
+          <Image
+            width={80}
+            height={80}
+            className="mt-3 w-[100px] md:m-0 md:w-[80px]"
+            src="/assets/icons/inner-city-logo.png"
+            alt=""
+          />
+        </div>
       </div>
-       <div className="md:flex p-[20px] hidden justify-between gap-10 items-center bg-blue ">
-     
-      
-      <div>
-        <Image width={80} height={80} className="w-[80px]" src="/assets/images/Logoi.png" alt="" />
-      </div>
-      <ul className="flex gap-[35px] text-white">
-        <Link href='/'>Home</Link>
+      {!isNavOpen ||
+        (isMobile && (
+          <div className="relative h-[100vh] bg-primary p-4 transition-all md:flex md:h-auto md:items-center md:justify-center md:bg-transparent md:p-0">
+            <ul className=" flex flex-col md:items-center gap-[35px] text-white text-sm font-bold  md:flex-row md:gap-[15px]">
+              <div>
+                <Link href="/">Home</Link>
+              </div>
 
-        <Link href="/about">About us</Link>
-        <div className='flex gap-1 items-center'>
-        <Link href='/program'>What we do</Link>
-        <MdKeyboardArrowDown/>
-        </div>
-        <div className='flex gap-1 items-center'>
-        <Link href="/insights">Special Insightss</Link>
-        <MdKeyboardArrowDown/>
-        </div>
-        <div className='flex gap-1 items-center'>
-        <Link href="/about">Support Project</Link>
-        <MdKeyboardArrowDown/>
-        </div>
-      <li>Blog</li>
-      </ul>
-      <div className="flex gap-4 justify-center items-baseline align-baseline">
-        <div>
-          <CiSearch color='white'/>
-        </div>
-        <div>
-        <button className="bg-white pt-2 pb-2 pl-5 pr-5 rounde-sm text-sm font-bold">Donate</button>
-        </div>
-       
-      </div>
+              <div>
+                <Link href="/about">About us</Link>
+              </div>
+
+              <div className="flex items-center gap-1">
+                <Link href="/program">What we do</Link>
+                <ChevronDownIcon color="white" className="w-7 text-white" />
+              </div>
+              <div className="flex items-center gap-1">
+                <Link href="/insights">Special Insights</Link>
+                <ChevronDownIcon color="white" className="w-7 text-white" />
+              </div>
+              <div className="flex items-center gap-1">
+                <Link href="/about">Support Project</Link>
+                <ChevronDownIcon color="white" className="w-7 text-white" />
+              </div>
+              <li>Blog</li>
+            </ul>
+            <div className=" flex items-center justify-normal gap-4 pt-10 align-baseline md:ml-32 md:justify-center md:pt-0">
+              <div className="order-2 md:order-1">
+                <SearchIcon width="30px" height="30px" color="white" />
+              </div>
+              <div>
+                <button className="rounded-md bg-white pb-2 pl-5 pr-5 pt-2  text-sm font-bold">
+                  Donate
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
     </div>
-    </div>
-   
   )
 }
