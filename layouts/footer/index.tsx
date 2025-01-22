be89b1a4254cd796bd8ec8b6b41a46bed24239f3
ryanfavour4 <ryanfave022@gmail.com>
2025-01-22 18:26:08 +0100

    Add social media icons and enhance footer layout

--- layouts/footer/index.tsx
+++ layouts/footer/index.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import logo from '@/public/assets/icons/logo-white.png'
+import fbIcon from '@/public/assets/icons/facebook-icon-white.png'
+import kcIcon from '@/public/assets/icons/kingschat-icon-white.png'
+import igIcon from '@/public/assets/icons/instagram-icon-white.png'
+import twIcon from '@/public/assets/icons/twitter-icon-white.png'
+import lnIcon from '@/public/assets/icons/linkeln-icon-white.png'
 
 export default function Footer() {
   return (
@@ -13,7 +18,7 @@ export default function Footer() {
             <Image src={logo.src} alt="logo" className="w-24 md:w-28" width={100} height={50} />
           </Link>
 
-          <div className="flex flex-col gap-1">
+          <div className="flex flex-col gap-2 text-sm">
             <span className="flex items-center gap-2">
               <p className="font-semibold">Email:</p>{' '}
               <a href="mailto:info@theinnercitymission.ngo">info@theinnercitymission.ngo</a>
@@ -31,8 +36,8 @@ export default function Footer() {
         </div>
         {/*  */}
         <div className="">
-          <h4>Ways to help</h4>
-          <div className="">
+          <h4 className="mb-6 text-lg font-semibold">Ways to help</h4>
+          <div className="flex flex-col gap-2 text-sm">
             <Link href={'/'}>Instant Donation</Link>
             <Link href={'/'}>Give Monthly</Link>
             <Link href={'/'}>Become a Volunteer</Link>
@@ -42,23 +47,49 @@ export default function Footer() {
         </div>
         {/*  */}
         <div className="">
-          <h4>Ways to help</h4>
-          <div className="">
-            <Link href={'/'}>Instant Donation</Link>
-            <Link href={'/'}>Give Monthly</Link>
-            <Link href={'/'}>Become a Volunteer</Link>
-            <Link href={'/'}>Sponsor Campaigns</Link>
-            <Link href={'/'}>Pray with Us</Link>
+          <h4 className="mb-6 text-lg font-semibold">Quick Links</h4>
+          <div className="flex flex-col gap-2 text-sm">
+            <Link href={'/'}>Gifting Campaigns</Link>
+            <Link href={'/'}>Home</Link>
+            <Link href={'/'}>About Us</Link>
+            <Link href={'/'}>Blog</Link>
           </div>
         </div>
         {/*  */}
-        <div className="col-span-2 md:col-span-2">
-          <h3>Subscribe to our Newsletter</h3>
+        <div className="col-span-2 flex flex-col gap-5 md:col-span-2">
+          <h3 className="text-xl font-semibold md:text-2xl">Subscribe to our Newsletter</h3>
 
-          <form className="">
-            <input type="text" />
-            <button className="btn-white">Subscribe</button>
+          <form className="flex overflow-hidden rounded-md border-2 border-light bg-transparent ring-light/50 hover:ring-2 focus:ring-2 active:ring-2">
+            <input
+              type="text"
+              placeholder="Your email address"
+              className="w-full border-none bg-transparent outline-none focus:outline-none"
+            />
+            <button className="btn-white w-fit">Subscribe</button>
           </form>
+        </div>
+      </div>
+      <div className="mt-10 flex flex-col items-center border-t border-light pt-6">
+        <small className="mt-10 block text-sm">
+          © {new Date().getFullYear()}. The InnerCity Mission for Children NGO. All Rights
+          Reserved. See our Privacy Policy.
+        </small>
+        <div className="mt-8 flex gap-4">
+          <a target="_blank" href="http://facebook.com">
+            <Image src={fbIcon} alt="fbIcon" width={30} height={30} className="object-contain" />
+          </a>
+          <a target="_blank" href="http://kingschat.online/">
+            <Image src={kcIcon} alt="kcIcon" width={35} height={35} className="object-fill" />
+          </a>
+          <a target="_blank" href="http://instagram.com">
+            <Image src={igIcon} alt="igIcon" width={30} height={30} className="object-fill" />
+          </a>
+          <a target="_blank" href="http://x.com">
+            <Image src={twIcon} alt="twIcon" width={30} height={30} className="object-fill" />
+          </a>
+          <a target="_blank" href="http://linkedin.com">
+            <Image src={lnIcon} alt="lnIcon" width={30} height={30} className="object-fill" />
+          </a>
         </div>
       </div>
     </footer>
--- next-env.d.ts
+++ next-env.d.ts
@@ -3,4 +3,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
-declare module 'tailwind-clip-path'
