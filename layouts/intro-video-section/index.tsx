e614a25e9818d49ce0b2e11aa25a2de59b7d32e2
ryanfavour4 <ryanfave022@gmail.com>
2025-10-07 07:32:38 +0100

    `Update various files with new Vimeo video IDs and minor styling changes`

--- layouts/intro-video-section/index.tsx
+++ layouts/intro-video-section/index.tsx
@@ -3,13 +3,13 @@ import { VideoCard } from '@/components/video-card'
 
 function IntroVideoSection() {
   return (
-    <div className="relative min-h-20 overflow-hidden bg-light py-10">
+    <div className="relative min-h-20 overflow-hidden bg-secondary pt-8 md:py-10">
       <div className="wrapper flex items-center justify-center">
         {/* /* BLUE HALF CIRCLE BACKDROP */}
         {/* <div className="absolute -mt-[60%] flex aspect-1 w-dvw flex-col items-center justify-center rounded-full bg-secondary" /> */}
         <div className="absolute flex aspect-1 w-dvw flex-col items-center justify-center bg-secondary" />
         <div className="relative z-10 flex w-full flex-col items-center justify-center text-textcolor">
-          <div className="mb-16 flex flex-col items-center justify-center gap-2 px-4 text-center md:mt-10">
+          <div className="mb-0 flex flex-col items-center justify-center gap-2 px-4 text-center md:mb-16 md:mt-10">
             <h2 className="text-2xl font-semibold md:text-3xl">— ICM SCHOOL MEALS —</h2>
             <p>Help a child learn by giving the gift of a daily meal.</p>
           </div>
