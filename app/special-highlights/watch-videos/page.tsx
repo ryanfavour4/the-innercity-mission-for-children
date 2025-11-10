aaaca311222e8f9c69f65c7c9cff04511c092bce
ryanfavour4 <ryanfave022@gmail.com>
2025-11-10 14:03:18 +0100

    `Added dynamic import and 'use client' directive`

--- app/special-highlights/watch-videos/page.tsx
+++ app/special-highlights/watch-videos/page.tsx
@@ -1,4 +1,6 @@
 'use client'
+export const dynamic = 'force-dynamic'
+
 import React, { useEffect, useState } from 'react'
 import Input from '@/components/input'
 import countries from '@/json/countries-list.json'
