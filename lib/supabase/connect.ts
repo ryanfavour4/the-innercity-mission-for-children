d7ba7fceb83ce0a0f7cd1a21b317e3bb2c03982e
ryanfavour4 <ryanfave022@gmail.com>
2025-11-14 02:29:03 +0100

    `Added Suspense component and removed console logs`

--- lib/supabase/connect.ts
+++ lib/supabase/connect.ts
@@ -2,6 +2,4 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-console.log(supabaseUrl)
-console.log(supabaseAnonKey)
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
