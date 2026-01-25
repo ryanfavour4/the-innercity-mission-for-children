8b5e591d0341137c429421b71e937fa3ec4eea4e
ryanfavour4 <ryanfave022@gmail.com>
2026-01-25 23:30:18 +0100

    `Updated code with changes to API route, donate component, and image assets`

--- app/api/ip-location/route.ts
+++ app/api/ip-location/route.ts
@@ -5,7 +5,6 @@ export async function GET(req: NextRequest) {
   // Get user's IP
   const ip =
     req.headers.get('x-forwarded-for')?.split(',')[0] || req.headers.get('x-real-ip') || 'self' // fallback (IPFind supports "self")
-  console.log('THIS IS IP HELLOOO -----------', ip)
   const res = await fetch(`https://ipfind.co/?ip=${ip}&auth=${process.env.IPFIND_API_KEY}`)
 
   if (!res.ok) {
@@ -13,7 +12,7 @@ export async function GET(req: NextRequest) {
   }
 
   const data = await res.json()
-  console.log(res.json())
+
   return NextResponse.json(
     encryptServer({
       ...data,
