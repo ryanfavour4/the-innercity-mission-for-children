3d29f1f109747b0afcaf160f333ea913ef75cb70
ryanfavour4 <ryanfave022@gmail.com>
2026-02-05 17:06:44 +0100

    mov

--- services/course-training/constants.ts
+++ services/course-training/constants.ts
@@ -1,3 +1,3 @@
-// export const baseUrl = 'https://icm-course-training-backend.onrender.com/api'
-export const baseUrl = 'http://localhost:3939/api'
+export const baseUrl = 'https://icm-course-training-backend.onrender.com/api'
+// export const baseUrl = 'http://localhost:3939/api'
 export const kingsChatClientId = 'baed362b-3960-482b-9e44-f61d72001668'
