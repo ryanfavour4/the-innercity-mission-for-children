02e5887c0b2cd082e7b63e6cbf3c94134f7eb57f
ryanfavour4 <ryanfave022@gmail.com>
2026-05-04 23:27:34 +0100

    raphathon

--- app/special-highlights/livestream/page.tsx
+++ app/special-highlights/livestream/page.tsx
@@ -12,7 +12,7 @@ export default function LiveStreamPage() {
   const [email, setEmail] = useState({ value: '' })
   const [testimony, setTestimony] = useState({ value: '' })
   const [country, setCountry] = useState('NG')
-  const title = 'Global Communion Service With Pastor Chris'
+  const title = ' Rhapathon With Pastor Chris'
 
   const handleSubmitGemForm = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
